a76f72e7a1416835bdf1765d8549be45dcb81461
Grom <k0nstamtinus@gmail.com>
2023-08-10 03:26:51 +0600

    Update 5

--- tests/Task5.spec.ts
+++ tests/Task5.spec.ts
@@ -1,8 +1,8 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import {Blockchain, SandboxContract} from '@ton-community/sandbox';
 import {Cell, toNano} from 'ton-core';
-import { Task5 } from '../wrappers/Task5';
+import {Task5} from '../wrappers/Task5';
 import '@ton-community/test-utils';
-import { compile } from '@ton-community/blueprint';
+import {compile} from '@ton-community/blueprint';
 
 describe('Task5', () => {
     let code: Cell;
@@ -46,11 +46,18 @@ describe('Task5', () => {
         checkEq(2);
 
         res = await task5.getFibonacciSequence(201, 4);
-
         checkEq(453973694165307953197296969697410619233826);
         checkEq(734544867157818093234908902110449296423351);
         checkEq(1188518561323126046432205871807859915657177);
         checkEq(1923063428480944139667114773918309212080528);
+
+        res = await task5.getFibonacciSequence(0, 3);
+        checkEq(0);
+        checkEq(1);
+        checkEq(1);
+
+        res = await task5.getFibonacciSequence(2, 1);
+        checkEq(1);
 
     });
 });
