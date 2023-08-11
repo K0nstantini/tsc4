a783e7dafae75e46a4ecc00afa9ce590a71c25dc
Grom <k0nstamtinus@gmail.com>
2023-08-11 22:11:33 +0600

    Update 3

--- tests/Task3.spec.ts
+++ tests/Task3.spec.ts
@@ -130,27 +130,54 @@ describe('Task3', () => {
         // ============== 5 ===================================
 
 
-        let flag = 0b101 ;
-        let value = 0b111 ;
+        let flag = 0b11101;
+        let value = 0b11111;
 
-        const p = '011000010110001001100011';
-        let list = new Cell({
-            bits: new BitString(new Buffer(p, 'binary'), 0, p.length)
-        });
+        // const p = '011000010110001001100011';
+        // let list = new Cell({
+        //     bits: new BitString(new Buffer(p, 'binary'), 0, p.length)
+        // });
 
-        // let list = beginCell()
-        //     .storeUint(BigInt(0xA78D25596F489D9549DB2E8270068A85DF1BDC5ADE28D6118B2D127B91C3BCC308151D3AC81BD3A5B24807101CAB3BF63198A51FDA261AD6344FDB59112E7C46), 512)
-        //     .storeUint(BigInt(0x87A856616E4C0268CB77368D329C63D6B3E0E4DD2BC398696D44CC0DEC21FC7900E26F7DCF3976C3F039153E990F0B0FA6C67691DC5B4D843F7C63712E7C46), 504)
-        //     .storeUint(0, 7)
-        //     .endCell();
+        let list = beginCell()
+            .storeStringTail("Today, we are going to teach our prospective builders how to mine on TON Blockchain. This experience will allow all of you to u")
+            .storeUint(7, 7)
+            .storeRef(beginCell()
+                .storeStringTail("Most smart contracts should not perform non-trivial actions or reject the inbound message on receiving a \"simple transfer messa")
+                .storeUint(7, 7)
+                .storeRef(
+                    beginCell()
+                        .storeStringTail("In order to achieve this goal")
+                        .endCell()
+                )
+                .endCell()
+            )
+            .endCell();
 
+        // let ds = list.beginParse();
+        // console.log(ds.loadBits(1016).toString());
+        // console.log(getHex(ds));
+        // ds = ds.loadRef().beginParse();
+        // console.log(getHex(ds));
+        // ds = ds.loadRef().beginParse();
+        // console.log(getHex(ds));
 
-        // let s = list.beginParse().loadBits(p.length);
-        console.log(list);
 
         let res = await task3.getFindAndReplace(flag, value, list);
         let ds = res.beginParse();
-        expect(ds.loadUint(p.length)).toEqual(6414947);
-        console.log(ds);
+        expect(ds.loadBits(1016).toString()).toEqual("546FE461792C207F65206172652067EFE96E67207C6F207C65616368206FF5722070726FF37065637C697E6520627D696C6465727320686FF7207C6F206D696E65206FEE20544FCE20426C6FE36B636861696E2E205468697320657870657269656E6365207F696C6C20616C6C6FF720616C6C206FE620796FF5207C6F207D");
+        expect(ds.loadUint(7)).toEqual(7);
+        ds = ds.loadRef().beginParse();
+        expect(ds.loadBits(1016).toString()).toEqual("CD6FF37C20736D61727C20636FEE7C7261637C732073686FF56C64206E6FF420706572666FF26D206E6FEE2D7C72697E69616C2061637C696FEE73206FF22072656A65637C207C686520696E626FF56E64206D6573736167E5206FEE2072656365697E696E672061202273696D706C65207C72616E73666572206D65737361");
+        expect(ds.loadUint(7)).toEqual(7);
+        ds = ds.loadRef().beginParse();
+        expect(ds.loadBits(232).toString()).toEqual("C96E206FF2646572207C6F2061636869657E65207C6869732067EFE16C");
     });
 });
+
+function getHex(ds: Slice) {
+    let str = '';
+    while (ds.remainingBits >= 8) {
+        str += String.fromCharCode(parseInt(ds.loadBits(8).toString(), 16));
+    }
+    return str;
+}
